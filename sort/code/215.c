2bef6d5d777a39f690716c75c3fdc08004ad10b7
cxy <37500268+chenxygh@users.noreply.github.com>
2020-01-14 10:17:59 +0800

    215 modifying

--- sort/code/215.c
+++ sort/code/215.c
@@ -1,26 +1,67 @@
-int partition(int *nums, int low, int high) {
-    int pivot = nums[low];
-    while (low < high) {
-        while (low < high && nums[high] <= pivot) --high;
-        nums[low] = nums[high];
-        while (low < high && nums[low] >= pivot) ++low;
-        nums[high] = nums[low];
-    }
-    nums[low] = pivot;
+#include <stdio.h>
 
-    return low;
+int partition(int *nums, int low, int high) {
+	int pivot = nums[low];
+	while (low < high) {
+		while (low < high && nums[high] <= pivot) --high;
+		nums[low] = nums[high];
+		while (low < high && nums[low] > pivot) ++low;
+		nums[high] = nums[low];
+	}
+	nums[low] = pivot;
+
+	return low;
+}
+
+// int findKthLargest(int* nums, int numsSize, int k){
+//     int low = 0;
+//     int high = numsSize - 1;
+//     int pivotPos = partition(nums, low, high);
+
+//     while ((pivotPos + 1) != k) {
+//         if ((pivotPos + 1) < k) low = pivotPos + 1;
+//         else if ((pivotPos + 1) > k) high = pivotPos - 1;
+//         pivotPos = partition(nums, low, high);
+//     }
+
+//     return nums[pivotPos];
+// }
+
+
+void adjustUp (int nums[], int k, int low) {
+    int temp = nums[low + k - 1];
+    for (int i = k / 2; i >= 1; i = k / 2) {
+        if (temp >= nums[low + i - 1]) break;
+        nums[low + k - 1] = nums[low + i - 1];
+        k = i;
+    }
+    nums[low + k - 1] = temp;
+}
+
+void insertHeap (int heap[], int x, int *n, int low) {
+    heap[(*n)++] = x;
+    adjustUp (heap, *n - low, low);
 }
 
 int findKthLargest(int* nums, int numsSize, int k){
+    int n = 0;
     int low = 0;
-    int high = numsSize - 1;
-    int pivotPos = partition(nums, low, high);
-
-    while ((pivotPos + 1) != k) {
-        if ((pivotPos + 1) < k) low = pivotPos + 1;
-        else if ((pivotPos + 1) > k) high = pivotPos - 1;
-        pivotPos = partition(nums, low, high);
+    int heap[numsSize];
+    for (int i = 0; i < numsSize; ++i) {
+        insertHeap (heap, nums[i], &n, low);
+        if (n >= (k + 1)) {
+            ++low;
+        }
     }
 
-    return nums[pivotPos];
+    return (low + 1) < numsSize && heap[low] > heap[low + 1] ? heap[low + 1]: heap[low];
+}
+
+int main(int argc, char const *argv[])
+{
+	int nums[] = {3, 1, 2, 4};
+
+	printf("%d\n", findKthLargest (nums, 4, 2));
+
+	return 0;
 }
