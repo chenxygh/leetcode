25ebe47ffda19b53a931a6997802d97a7871aaf8
cxy <37500268+chenxygh@users.noreply.github.com>
2020-01-17 09:17:56 +0800

    102_二叉树的层次遍历 done

--- sort/code/80.c
+++ sort/code/80.c
@@ -1,44 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
-
-
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
-int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes){
-    int **res = (int **)malloc(sizeof(int *) * 1000);
-    returnColumnSizes[0] = (int *)malloc(sizeof(int) * 1000);
-    int *nums = (int *)malloc(sizeof(int) * 10000);
-    struct TreeNode * Queue[10000];
-    int front, rear, level, last;
-    front = rear = level = 0;
-    int n = 0;
-    int cnt = 0;
-    Queue[root? rear++: rear] = root;
-    last = rear;
-    while (front < rear) {
-        struct TreeNode *p = Queue[front++];
-        cnt++;
-        nums[n++] = p->val;
-        if (p->left) Queue[rear++] = p->left;
-        if (p->right) Queue[rear++] = p->right;
-
-        if (front == last) {
-            last = rear;
-            returnColumnSizes[0][level] = cnt;
-            res[level++] = nums + n - cnt;
-            cnt = 0;
+int removeDuplicates(int* nums, int numsSize){
+    int i = 0;
+    for (int j = 1, cnt = 1; j < numsSize;) {
+        if (nums[i] != nums[j]) {
+            cnt = 1;
+            nums[++i] = nums[j++];
+        } else if (++cnt > 2) {
+            cnt = 1;
+            while (j < numsSize && nums[i] == nums[j]) ++j;
+        } else {
+            nums[++i] = nums[j++];
         }
     }
 
-    returnSize[0] = level;
-    return res;
+    return numsSize? i + 1: 0;
 }
