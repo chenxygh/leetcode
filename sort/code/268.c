a24aac24bc4644552bca225913624087ac9caa76
cxy <37500268+chenxygh@users.noreply.github.com>
2020-02-27 16:05:24 +0800

    268_缺失数字 done

---
+++ sort/code/268.c
@@ -0,0 +1,21 @@
+// int missingNumber(int* nums, int numsSize){
+//     char dict[numsSize + 1];
+//     for (int i = 0; i < numsSize + 1; ++i) dict[i] = 0;
+//     for (int i = 0; i < numsSize; ++i) dict[nums[i]] = 1;
+//     for (int i = 0; i < numsSize + 1; ++i) if (dict[i] == 0) return i;
+//     return 0;
+// }
+
+// int missingNumber(int* nums, int numsSize){
+//     int res = 0;
+//     for (int i = 0; i < numsSize; ++i) res ^= i ^ nums[i];
+    
+//     return res ^ numsSize;
+// }
+
+int missingNumber(int* nums, int numsSize){
+    int sum = 0;
+    for (int i = 0; i < numsSize; ++i) sum += i - nums[i];
+    
+    return sum + numsSize;
+}
