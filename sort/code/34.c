7181fd774aa08106c2d322a84c3a5c7afb421d40
cxy <37500268+chenxygh@users.noreply.github.com>
2020-03-01 16:53:09 +0800

    34.c modified

--- sort/code/34.c
+++ sort/code/34.c
@@ -22,28 +22,54 @@
 //     return res;
 // }
 
+// int* searchRange(int* nums, int numsSize, int target, int* returnSize){
+//     returnSize[0] = 2;
+//     int *res = (int *)malloc(sizeof(int) * 2);
+//     res[0] = res[1] = -1;
+
+//     if (numsSize == 0) return res;
+
+//     int left = 0; 
+//     int right = numsSize - 1;
+//     int mid = 0;
+//     while (left <= right) {
+//         mid = (left + right) / 2;
+//         if (nums[mid] < target) left = mid + 1;
+//         else if (nums[mid] >= target) right = mid - 1;
+//     }
+//     printf("%d\n", mid);
+//     if (left < numsSize && nums[left] == target) {
+//         res[0] = left;
+//         while (left < numsSize && nums[left] == target) ++left;
+//         res[1] = left - 1;
+//     }
+
+//     return res;
+// }
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+// 无须走完
 int* searchRange(int* nums, int numsSize, int target, int* returnSize){
-    returnSize[0] = 2;
+    int left = 0, right = numsSize - 1;
     int *res = (int *)malloc(sizeof(int) * 2);
     res[0] = res[1] = -1;
-
-    if (numsSize == 0) return res;
-
-    int left = 0; 
-    int right = numsSize - 1;
-    int mid = 0;
+    returnSize[0] = 2;
     while (left <= right) {
-        mid = (left + right) / 2;
+        int mid = left + (right - left) / 2;
         if (nums[mid] < target) left = mid + 1;
-        else if (nums[mid] >= target) right = mid - 1;
+        else if (nums[mid] > target) right = mid - 1;
+        else {
+            int n = mid;
+            for (; n >= 0 && nums[n] == target; --n);
+            res[0] = n >= 0? n + 1: 0;
+            n = mid + 1;
+            for (; n < numsSize && nums[n] == target; ++n);
+            res[1] = n < numsSize? n - 1: numsSize - 1;
+            return res;
+        }
     }
-    printf("%d\n", mid);
-    if (left < numsSize && nums[left] == target) {
-        res[0] = left;
-        while (left < numsSize && nums[left] == target) ++left;
-        res[1] = left - 1;
-    }
-
     return res;
 }
 
