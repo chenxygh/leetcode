3bb3aa3c8a30574af9393aa48c5af33e0a810827
cxy <37500268+chenxygh@users.noreply.github.com>
2020-03-22 17:01:37 +0800

    226 modified

--- sort/code/226.c
+++ sort/code/226.c
@@ -7,14 +7,61 @@
  * };
  */
 
+// post order
+// struct TreeNode* mirrorTree(struct TreeNode* root){
+//     if (root) {
+//         mirrorTree (root->left);
+//         mirrorTree (root->right);
+//         struct TreeNode *temp = root->left;
+//         root->left = root->right;
+//         root->right = temp;
+//     }
+//     return root;
+// }
 
-struct TreeNode* invertTree(struct TreeNode* root){
-    if (root) {
-        invertTree (root->left);
-        invertTree (root->right);
-        struct TreeNode *p = root->left;
-        root->left = root->right;
-        root->right = p;
+// pre order
+// struct TreeNode* mirrorTree(struct TreeNode* root){
+//     if (root) {
+//         struct TreeNode *temp = root->left;
+//         root->left = root->right;
+//         root->right = temp;
+//         mirrorTree (root->left);
+//         mirrorTree (root->right);
+//     }
+//     return root;
+// }
+
+// in order
+// struct TreeNode* mirrorTree(struct TreeNode* root){
+//     if (root) {
+//         mirrorTree (root->left);
+//         struct TreeNode *temp = root->left;
+//         root->left = root->right;
+//         root->right = temp;
+//         mirrorTree (root->left);
+//     }
+//     return root;
+// }
+
+// level order
+struct TreeNode* mirrorTree(struct TreeNode* root){
+    struct TreeNode *Queue[100];
+    int front = 0, rear = 0;
+    Queue[root? rear++: rear] = root;
+
+    while (front < rear) {
+        struct TreeNode *p = Queue[front++];
+        // 先存再交换， 与先交换再存， 无所谓， 皆可
+        // if (p->left) Queue[rear++] = p->left;
+        // if (p->right) Queue[rear++] = p->right;
+
+        struct TreeNode *temp = p->left;
+        p->left = p->right;
+        p->right = temp;
+
+        if (p->left) Queue[rear++] = p->left;
+        if (p->right) Queue[rear++] = p->right;
     }
+
     return root;
 }
