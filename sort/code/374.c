3c481ac6d0cbf9601c5ecd7c66b2368fa80fad9b
cxy <37500268+chenxygh@users.noreply.github.com>
2020-03-22 14:43:17 +0800

    374_猜数字大小 done

---
+++ sort/code/374.c
@@ -0,0 +1,22 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+int guessNumber(int n){
+    int left = 1, right = n;
+    int returnGuess = 0;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        returnGuess = guess(mid);
+        if (returnGuess == 1) left = mid + 1;
+        else if (returnGuess == -1) right = mid - 1;
+        else return mid;
+    }
+
+    return 0;
+}
