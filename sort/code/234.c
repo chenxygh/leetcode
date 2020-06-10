18fafc42362ce93f0764335ddfefe7e51ab391c8
cxy <37500268+chenxygh@users.noreply.github.com>
2020-06-10 11:30:35 +0800

    234 modified code

--- sort/code/234.c
+++ sort/code/234.c
@@ -7,23 +7,23 @@
  */
 
 
-struct ListNode* partition(struct ListNode* head, int x){
-    if (head == NULL || head->next == NULL) return head;
-    struct ListNode *l1 = (struct ListNode *)malloc (sizeof(struct  ListNode)), *rear1 = l1;
-    struct ListNode *l2 = (struct ListNode *)malloc (sizeof(struct  ListNode)), *rear2 = l2;
-    l1->next = l2->next = NULL;
-    while (head) {
-        if (head->val < x) {
-            rear1 = rear1->next = head;
-        } else {
-            rear2 = rear2->next = head;
-        }
-        head = head->next;
+bool isPalindrome(struct ListNode* head){
+    if (head == NULL || head->next == NULL) return true;
+    struct ListNode *p = head, *q = head, *L = (struct ListNode *) malloc (sizeof(struct ListNode));
+    L->next = NULL;
+    while (q && q->next) {
+        struct ListNode *temp = p;
+        p = p->next;
+        q = q->next->next;
+        temp->next = L->next;
+        L->next = temp;
     }
-    rear1->next = l2->next;
-    rear2->next = NULL;
-    head = l1->next;
-    free(l1);
-    free(l2);
-    return head;
+    if (q) p = p->next;
+    q = L->next;
+    free(L);
+    while (p && p->val == q->val) {
+        p = p->next;
+        q = q->next;
+    }
+    return p == NULL;
 }
